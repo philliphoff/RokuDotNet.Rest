23fd6afa347c727556f21b2eddc48cb0b565a6b9
Phillip Hoff <phillip@orst.edu>
2018-04-25 23:42:39 -0700

    Add more query APIs.

--- src/RokuDotNet.Rest/RokuController.cs
+++ src/RokuDotNet.Rest/RokuController.cs
@@ -16,12 +16,44 @@ namespace RokuDotNet.Rest
             this.deviceProvider = deviceProvider ?? throw new ArgumentNullException(nameof(deviceProvider));
         }
 
+        [Route("devices/{id}/query/apps")]
+        public async Task<GetAppsResult> GetAppsAsync(string id)
+        {
+            var device = await this.deviceProvider.GetDeviceFromIdAsync(id);
+
+            return await device.QueryApi.GetAppsAsync();
+        }
+
+        [Route("devices/{id}/query/active-app")]
+        public async Task<GetActiveAppResult> GetActiveAppAsync(string id)
+        {
+            var device = await this.deviceProvider.GetDeviceFromIdAsync(id);
+
+            return await device.QueryApi.GetActiveAppAsync();
+        }
+
         [Route("devices/{id}/query/device-info")]
         public async Task<DeviceInfo> GetDeviceInfoAsync(string id)
         {
             var device = await this.deviceProvider.GetDeviceFromIdAsync(id);
 
             return await device.QueryApi.GetDeviceInfoAsync();
+        }
+ 
+        [Route("devices/{id}/query/tv-channels")]
+        public async Task<GetTvChannelsResult> GetTvChannelsAsync(string id)
+        {
+            var device = await this.deviceProvider.GetDeviceFromIdAsync(id);
+
+            return await device.QueryApi.GetTvChannelsAsync();
+        }
+
+        [Route("devices/{id}/query/tv-active-channel")]
+        public async Task<GetActiveTvChannelResult> GetActiveTvChannelAsync(string id)
+        {
+            var device = await this.deviceProvider.GetDeviceFromIdAsync(id);
+
+            return await device.QueryApi.GetActiveTvChannelAsync();
         }
     }
 }
