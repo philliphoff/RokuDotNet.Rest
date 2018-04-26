ab10642c5f6d41ebd934a096115c4a9b40b91a37
Phillip Hoff <phillip@orst.edu>
2018-04-25 22:29:37 -0700

    Initial commit.

---
+++ src/RokuDotNet.Rest/RokuController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RokuDotNet.Client;
+using RokuDotNet.Client.Query;
+using System;
+using System.Threading.Tasks;
+
+namespace RokuDotNet.Rest
+{
+    [Route("[controller]")]
+    public class RokuController : Controller
+    {
+        [Route("devices/{id}/query/device-info")]
+        public async Task<DeviceInfo> GetDeviceInfoAsync(string id)
+        {
+            var discoveryClient = new RokuDeviceDiscoveryClient();
+
+            var device = await discoveryClient.DiscoverSpecificDeviceAsync($"uuid:roku:ecp:{id}");
+            var deviceInfo = await device.QueryApi.GetDeviceInfoAsync();
+
+            return deviceInfo;
+        }
+
+        [HttpGet("test")]
+        public Task<string> Test()
+        {
+            return Task.FromResult("Hello world!");
+        }
+    }
+}
